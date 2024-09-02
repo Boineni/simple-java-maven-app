59746c5f1a89fb59a0f7bd742117be3b7502580b
Boineni <78819238+Boineni@users.noreply.github.com>
2024-09-02 19:17:31 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Naarsoft!";
+    private final String message = "Hello Naarsoft1234!";
 
     public App() {}
 
